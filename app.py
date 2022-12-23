2a7226f3d7b09da11825b73f2f9cf85d8fa8ebf9
Boris <32761598+oooprya@users.noreply.github.com>
2022-12-23 16:55:16 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 import urllib.request, json 
-with urllib.request.urlopen("http://tv.tenet.ua/iptv/all11/ajax.php?page=film&parentid=n&pagenum=0&keyword=&order=2&desc=0&moreparam=gofilm&firstvizov=2&interval=16&pravoobladatelid=1&canplayhls=1&hotelid=0&devtype=json&macaddr=3C:BD:3E:C6:62:60&agent=AndroidTV&serialnum=35464022664247&ipadress=192.168.100.42&sid=9SF4VfheFfeBiup9PsAkm2laboTtSDmv&api_version=2") as url:
+with urllib.request.urlopen("http://tv.tenet.ua/iptv/all11333333554417/ajax.php?page=film&parentid=n&pagenum=0&keyword=&order=2&desc=0&moreparam=gofilm&firstvizov=2&interval=16&pravoobladatelid=1&canplayhls=1&hotelid=0&devtype=json&macaddr=3C:BD:3E:C6:62:60&agent=AndroidTV&serialnum=35464022664247&ipadress=192.168.100.42&sid=9SF4VfheFfeBiup9PsAkm2laboTtSDmv&api_version=2") as url:
     data = json.load(url)
 film_name = data['name']
 url_img = data['FLM_PIC_S']
