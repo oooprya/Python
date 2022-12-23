a5a9d0c823ee45756089bef3dfd98fe3e593364d
Boris <32761598+oooprya@users.noreply.github.com>
2022-12-23 08:51:21 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import urllib.request, json 
-with urllib.request.urlopen("http://api_version=2") as url:
+with urllib.request.urlopen("http://api_version=211") as url:
     data = json.load(url)
 
 for i in range(16):
     print(f"Название фильма: {data['name'][i]} img: {data['FLM_PIC_S'][i]}")
+
+
+print("Отобразить 16 фильмов")
